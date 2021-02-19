9f66f4ecd3794b01a98834685c289e8e2fb81e9d
Jared Deckard <jared@shademaps.com>
2021-02-19 11:25:28 -0600

    Fix browser-env Event scope test issue

--- test/watch.js
+++ test/watch.js
@@ -2,15 +2,15 @@ import test from 'ava';
 import browserEnv from 'browser-env';
 import { Mask } from '../src/index';
 
-browserEnv(['document', 'Event']);
+browserEnv(['window']);
 
 test('watch an input', t => {
   const mask = new Mask(/static string/);
-  const input = document.createElement('input');
+  const input = window.document.createElement('input');
   mask.watch(input);
 
   input.value = 'staticstring';
-  const event = new Event('input', {bubbles: true});
+  const event = new window.Event('input', {bubbles: true});
   input.dispatchEvent(event);
 
   t.is(input.value, 'static string');
